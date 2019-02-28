bac71a76edd671c7c2e7a45bf3eabfc24d9386d7
David Tolnay <dtolnay@gmail.com>
2019-02-28 00:23:03 -0800

    Write crate-level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,53 @@
+//! **Pull in every source file in a directory as a module.**
+//!
+//! # Syntax
+//!
+//! ```
+//! # const IGNORE: &str = stringify! {
+//! automod::dir!("path/to/directory");
+//! # };
+//! ```
+//!
+//! This macro expands to one or more `mod` items, one for each source file in
+//! the specified directory.
+//!
+//! The path is given relative to the directory containing Cargo.toml.
+//!
+//! It is an error if the given directory contains no source files.
+//!
+//! # Example
+//!
+//! Suppose that we would like to keep a directory of regression tests for
+//! individual numbered issues:
+//!
+//! - tests/
+//!   - regression/
+//!     - issue1.rs
+//!     - issue2.rs
+//!     - ...
+//!     - issue128.rs
+//!
+//! We would like to be able to toss files in this directory and have them
+//! automatically tested, without listing them in some explicit list of modules.
+//! Automod solves this by adding *tests/regression/mod.rs* containing:
+//!
+//! ```
+//! # const IGNORE: &str = stringify! {
+//! automod::dir!("tests/regression");
+//! # };
+//! ```
+//!
+//! The invocation expands to:
+//!
+//! ```
+//! # const IGNORE: &str = stringify! {
+//! mod issue1;
+//! mod issue2;
+//! /* ... */
+//! mod issue128;
+//! # };
+//! ```
+
 extern crate proc_macro;
 
 mod error;
