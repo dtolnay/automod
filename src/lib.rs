8e94b416f6c9ed608befa12f335e542bebebbe8e
David Tolnay <dtolnay@gmail.com>
2023-07-02 13:07:33 -0700

    Release 1.0.9

--- src/lib.rs
+++ src/lib.rs
@@ -58,7 +58,7 @@
 //! # };
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/automod/1.0.8")]
+#![doc(html_root_url = "https://docs.rs/automod/1.0.9")]
 #![allow(clippy::enum_glob_use, clippy::needless_pass_by_value)]
 
 extern crate proc_macro;
